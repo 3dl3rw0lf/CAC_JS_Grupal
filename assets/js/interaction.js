2ada1db0684c0424f59430905580439738242d0e
Mariana <marianabsanchez@hotmail.com>
2023-11-01 20:21:39 -0300

    Primer commit

--- assets/js/interaction.js
+++ assets/js/interaction.js
@@ -19,13 +19,52 @@ btnResumen.addEventListener("click", function() {
   
   const elementoNombre = document.getElementById("input-nombre");
   const elementoApellido = document.getElementById("input-apellido");
-  
-  if (elementoNombre !== null && elementoApellido !== null) {
-    const nombre = elementoNombre.value;
-    const apellido = elementoApellido.value;
-    console.log(nombre);
-    console.log(apellido);
+  const elementoMail= document.getElementById("input-correo");
+  const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3,4})+$/;
+
+  const nombre = elementoNombre.value;
+  const apellido = elementoApellido.value;
+  const mail= elementoMail.value;
+  if (elementoNombre !== null && elementoApellido !== null) { 
+    console.log('el nombre es' + nombre);
+    console.log('el apellido es' + apellido);
   } 
+  if (nombre==" " || nombre.length == 0 || /^\s+$/.test(nombre) || apellido==" "|| 
+      apellido.length == 0 || mail==" " ){//|| !(emailRegex.test(mail))){
+    if (nombre==" " || nombre.length == 0 || /^\s+$/.test(nombre)){
+        document.getElementById('aviso-nombre').innerHTML='Debe ingresar un nombre válido';
+    }
+    if (apellido==" " || apellido.length == 0 || /^\s+$/.test(apellido)){
+        document.getElementById('aviso-apellido').innerHTML='Debe ingresar un apellido válido';
+    }
+    //console.log(emailRegex.test(mail.value));
+    if (mail==" " || mail.length == 0 ){//|| !(emailRegex.test(mail)) ){
+        document.getElementById('aviso-mail').innerHTML='Debe ingresar un correo electrónico válido';
+    }
+  }
+  else{
+    document.getElementById('aviso-nombre').innerHTML="";
+    document.getElementById('aviso-apellido').innerHTML="";
+    document.getElementById('aviso-mail').innerHTML="";
+    let resultado=0;
+    let categoria= document.getElementById('input-categoria').value;
+    let cant = document.getElementById('input-cantidad').value;
+        switch(categoria){
+            case "80":
+                resultado=200*0.8*parseInt(cant);
+                break;
+            case "50":
+                resultado=200*0.5*parseInt(cant);
+                break;
+            case "15":
+                resultado=200*0.15*parseInt(cant);
+                break;
+            default:
+                resultado=200 *parseInt(cant);
+                break;
+        }
+    document.getElementById('Total').innerHTML= "Total a pagar: $ " + resultado;
+  }
 });
 
 
@@ -38,8 +77,11 @@ btnBorrar.addEventListener("click", function() {
   const inputApellido = document.getElementById("input-apellido");
   const inputCorreo = document.getElementById("input-correo");
   
-  
   inputNombre.value = "";
   inputApellido.value = "";
   inputCorreo.value = "";
+  document.getElementById("Total").innerHTML='Total a pagar: $';  
+  document.getElementById('aviso-nombre').innerHTML="";
+  document.getElementById('aviso-apellido').innerHTML="";
+  document.getElementById('aviso-mail').innerHTML="";
 });
